9dc21e86be8c32627c39d6a25dfca1b1eb6192bd
坚果 <3772548978@qq.com>
2026-03-25 18:37:38 +0800

    chore: 删除Emscripten平台相关的演示文件和依赖项
    移除Emscripten平台的演示文件、构建产物以及不再需要的node_modules依赖项

--- platform/Emscripten/demo/dist/index.js
+++
@@ -1,7 +0,0 @@
-export { SweetEditor } from './SweetEditor.js';
-export { Document } from './Document.js';
-export { ProtocolDecoder } from './ProtocolDecoder.js';
-export { ProtocolEncoder } from './ProtocolEncoder.js';
-export { CanvasRenderer, defaultTheme } from './renderer/CanvasRenderer.js';
-export * from './types/EditorTypes.js';
-//# sourceMappingURL=index.js.map
--- platform/Emscripten/js/dist/index.js
+++
@@ -1,7 +0,0 @@
-export { SweetEditor } from './SweetEditor';
-export { Document } from './Document';
-export { ProtocolDecoder } from './ProtocolDecoder';
-export { ProtocolEncoder } from './ProtocolEncoder';
-export { CanvasRenderer, defaultTheme } from './renderer/CanvasRenderer';
-export * from './types/EditorTypes';
-//# sourceMappingURL=index.js.map
